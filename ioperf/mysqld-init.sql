9d096e2ae03d581a2542bc65f0c32f1df26a1eab
Yiyang Wu <toolmanp@tlmp.cc>
2025-11-18 13:59:01 +0800

    fix(ioperf): add mysqld necessary init scripts
    Signed-off-by: Yiyang Wu <toolmanp@tlmp.cc>

--- ioperf/mysqld-init.sql
+++ ioperf/mysqld-init.sql
@@ -1,5 +1,6 @@
 ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';
 CREATE USER 'root'@'%' IDENTIFIED BY 'root';
 ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'root';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';
 FLUSH PRIVILEGES;
 CREATE DATABASE test;
